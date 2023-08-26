7d21e2e86ddc1336594d2dd735bfd30b576200e1
Marcin Wiatr <devjoter@gmail.com>
2023-08-27 00:24:24 +0200

    added firebase auth

---
+++ chatapp/lib/services/auth/auth_gate.dart
@@ -0,0 +1,29 @@
+import 'package:chatapp/services/auth/login_or_register.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import '../../pages/home_page.dart';
+
+class AuthGate extends StatelessWidget {
+  const AuthGate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          // user logged in
+          if (snapshot.hasData) {
+            return const HomePage();
+          } else {
+            return const LoginOrRegister();
+          }
+
+          // user is not logged in
+        },
+      ),
+    );
+  }
+}
